eed59487f7b730d890bcd01b0bdf10836e09102a
ayssasadeghi <78102492+ayssasadeghi@users.noreply.github.com>
2021-01-27 13:27:20 -0500

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,17 @@
+/************************************************************************* HelloWorld
+*************************************************************************
+Ayssa Sadeghi
+01/27/2021
+CMSC255-003
+************************************************************************/
+
+public class Hello {
+public static void main(String[] args) {
+System.out.println("<Ayssa Sadeghi>") ;
+System.out.println("<Java HelloWorld>") ;
+System.out.println("<Lab 1>") ;
+System.out.println("<01/27/2021>") ;
+System.out.println("<CMSC255-003>") ;
+System.out.println();
+}
+}
